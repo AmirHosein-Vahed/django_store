421a500ad0533675ced3df779d9491c70344ef0f
AmirHosein-Vahed <vahed.ahv@gmail.com>
2022-09-08 12:44:55 +0430

    register -> Register

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-admin.site.register("Promotion")
-admin.site.register("Collection")
-admin.site.register("Product")
-admin.site.register("Customer")
-admin.site.register("Order")
-admin.site.register("OrderItem")
-admin.site.register("Address")
-admin.site.register("Cart")
-admin.site.register("CartItem")
+admin.site.Register("Promotion")
+admin.site.Register("Collection")
+admin.site.Register("Product")
+admin.site.Register("Customer")
+admin.site.Register("Order")
+admin.site.Register("OrderItem")
+admin.site.Register("Address")
+admin.site.Register("Cart")
+admin.site.Register("CartItem")
