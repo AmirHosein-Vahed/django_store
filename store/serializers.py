d47f571b091232e34bc6ef1ac3eff562472387de
AmirHosein-Vahed <vahed.ahv@gmail.com>
2022-09-09 21:47:27 +0430

    serializer of product and collection added to project

---
+++ store/serializers.py
@@ -0,0 +1,50 @@
+from decimal import Decimal
+
+from rest_framework import serializers
+
+from .models import Collection, Product
+
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title', 'products_count']
+
+    products_count = serializers.IntegerField( )
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
+        
+    
+    price_with_tax = serializers.SerializerMethodField('calc_tax')
+    collection = serializers.PrimaryKeyRelatedField(queryset=Collection.objects.all())
+    # collection = serializers.StringRelatedField( ) 
+    # collection = CollectionSerializer()
+    
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset = Collection.objects.all(),
+    #     view_name='collection-detail'
+    # )
+
+    def calc_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
+
+    
+    # def create(self, validated_data):
+    #     product = Product(**validated_data)
+    #     product.other = 1
+    #     product.save()
+    #     return product
+
+    # def update(self, instance, validated_data):
+    #     instance.unit_price = validated_data.get("unit_price")
+    #     instance.save()
+    #     return instance
+
+    # def validate(self, data):
+    #     if data["title"] == "test":
+    #         return serializers.ValidationError("test can't be title")
+    #     return data
