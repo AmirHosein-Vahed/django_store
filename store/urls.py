5fbd0ecba551894c2c79b0739451f6f238f6121c
AmirHosein-Vahed <vahed.ahv@gmail.com>
2022-09-09 21:49:26 +0430

    urls created for storefront and store app

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,12 @@
+from django.urls import path
+from . import views
 
 
 urlpatterns = [
+    path('products/', views.product_list, name='product-list'),
+    path('products/<int:pk>', views.product_detail, name='product-detail'),
+
+    path('collections/', views.collection_list, name='collection-list'),
+    path('collections/<int:pk>', views.collection_detail, name='collection-detail'),
 
 ]
--- storefront/urls.py
+++ storefront/urls.py
@@ -8,7 +8,7 @@ admin.site.index_title = "Admin"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls')),
-    # path('store/', include('store.urls')),
+    path('store/', include('store.urls')),
     # path('tags/', include('tags.urls')),
     path('__debug__/', include(debug_toolbar.urls))
 ]
